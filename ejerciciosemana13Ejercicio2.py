3ea38d0408dbc2518659a5ae145b3e65fb633b4c
MarRV17 <mariorivera172025@gmail.com>
2026-04-26 15:22:02 -0600

    ejercicio 2, Contador de positivos y negativos

---
+++ ejerciciosemana13Ejercicio2.py
@@ -0,0 +1,22 @@
+## 2. Contador de positivos y negativos
+## •	Con while, permite ingresar números hasta que el usuario escriba 0.
+## •	Dentro, usa if para contar cuántos son positivos y negativos.
+## •	Al final, usa un for para mostrar un resumen de resultados.
+
+positivos = 0
+negativos = 0
+
+while True:
+    num = int(input("Ingrese un número (0 para terminar): "))
+
+    if num == 0:
+        break
+
+    if num > 0:
+        positivos += 1
+    else:
+        negativos += 1
+
+for i in range(1):
+    print("Positivos:", positivos)
+    print("Negativos:", negativos)
